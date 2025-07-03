76450e81fca01ee202b7fef9208b308c4f41d7f9
Markies <122152114+markies07@users.noreply.github.com>
2025-07-03 19:44:06 +0800

    update linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ function Footer() {
                 <a href='https://github.com/markies07' target='_blank' rel='noopener noreferrer' className='cursor-pointer px-2 py-1 hover:underline'>GITHUB</a>
                 <a href='https://www.facebook.com/mark.naval07/' target='_blank' rel='noopener noreferrer' className='cursor-pointer px-2 py-1 hover:underline'>FACEBOOK</a>
                 <a href='https://www.tiktok.com/@.markdev?_t=ZS-8xYdf8HBtiK&_r=1' target='_blank' rel='noopener noreferrer' className='cursor-pointer px-2 py-1 hover:underline'>TIKTOK</a>
-                <a href='https://www.linkedin.com/in/mark-naval-68426a252/' target='_blank' rel='noopener noreferrer' className='cursor-pointer px-2 py-1 hover:underline'>LINKEDIN</a>
+                <a href='https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile' target='_blank' rel='noopener noreferrer' className='cursor-pointer px-2 py-1 hover:underline'>LINKEDIN</a>
             </div>
 
         </div>
